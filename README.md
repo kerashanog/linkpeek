b557535f0294db1af83779c9ad4b687da201431f
kerashanog <kerashanog@gmail.com>
2015-06-24 17:57:58 +0000

    Add validation fo Links and Comments

--- README.md
+++ README.md
@@ -4,9 +4,13 @@ Rails app keep link like Reddit
 User can: 
 * Sign up 
 * Add the link 
+
 And also 
+
 * Comment 
+
 Or 
+
 * Vote up/down link.
 
 Enjoy!
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,8 @@
 class Link < ActiveRecord::Base
   
+  validates :title, presence: true, length: {minimum: 5}
+  validates :url,  presence: true, length: {minimum: 1}
+  
   belongs_to :user
   acts_as_votable
   has_many :comments
